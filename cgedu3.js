795b67b1e35196a4c4ba0cb4048f0bfc1274984b
Your Name <rbarker1196@gmail.com>
2018-06-27 18:25:58 -0500

    added things

---
+++ cgedu3.js
@@ -0,0 +1,11 @@
+/*var gradYear = function() {
+
+prompt ('What year are you graduating?!');
+}
+
+gradYear();
+
+var gradMonth = function(){
+prompt ('What month are you graduating!?');
+}
+gradMonth();*/
